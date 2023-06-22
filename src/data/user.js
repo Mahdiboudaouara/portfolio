0c6516b7cac6b2df8253ff7a4e7934bb5a535c30
maahdi boudaouara <mboudaouara@mercarue.com>
2023-06-22 02:16:50 +0100

    first

--- src/data/user.js
+++ src/data/user.js
@@ -1,28 +1,27 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "MB.Portfolio",
+		name: "Mahdi Boudaouara",
+		email: "mahdi.boudaouara@supcom.tn",
 		logo: "../logo.png",
 	},
 
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		twitter: "https://twitter.com/MahdiBoudaouara",
+		github: "https://github.com/Mahdiboudaouara",
+		linkedin: "https://www.linkedin.com/in/mahdi-boudaouara/",
+		instagram: "https://www.instagram.com/mahdi.boudaouara/",
+		facebook: "https://www.facebook.com/profile.php?id=100087212690465",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "ICT Engineering Student At The Higher School Of Communications Of Tunis",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"As a dedicated and hardworking individual, I am always seeking new challenges that enable me to utilize my skills and knowledge. With a passion for DevOps and a strong work ethic, I have developed a deep understanding of the principles and practices that underpin this field. I am committed to staying up-to-date with the latest technologies and methodologies, and am constantly looking for opportunities to expand my skill set and stay at the forefront of the industry.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Mahdi Boudaouara A student At Sup'Com And a DevOps enthusiast.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -35,48 +34,30 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Auction website with DevOps approach",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Developed a microservices-based auction website. Implemented automated CI/CD pipelines on Jenkins. Created Helm charts. Utilized ingress and nginx to expose the microservices to external traffic. Utilized jUnit and Selenium for automated testing, Used Prometheus & Grafana for monitoring.",
+			logo: "../cs.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/Mahdiboudaouara",
 		},
 
 		{
-			title: "Project 2",
+			title: "Neurodata",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+				"A website for a startup, worked on it with a team of 4 as a backend developper.",
+			logo: "../neurodata.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://neurodata.tech/en/industries",
 		},
 
 		{
-			title: "Project 3",
+			title: "IHEC Project",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+				"A file management project, worked on it as a chef for team of 4 , this project was made for students from IHEC carthage",
+			logo: "../ihec.png",
 			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/Mahdiboudaouara",
 		},
 	],
 };
