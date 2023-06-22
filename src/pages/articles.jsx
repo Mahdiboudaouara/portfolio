f33f9cb5b0cd229cb5e53bfd061bb31502277742
maahdi boudaouara <mboudaouara@mercarue.com>
2023-06-22 17:44:24 +0100

    54dddd

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -60,7 +60,7 @@ const Articles = () => {
 											date={article().date}
 											title={article().title}
 											description={article().description}
-											link={"/article/" + (index + 1)}
+											link={"/portfolio/article/" + (index + 1)}
 										/>
 									</div>
 								))}
