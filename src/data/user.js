eee4f0a6b08d966d7332fbc6bc036e66dfd99e03
maahdi boudaouara <mboudaouara@mercarue.com>
2023-07-18 02:27:09 +0100

    ready

--- src/data/user.js
+++ src/data/user.js
@@ -21,7 +21,7 @@ const INFO = {
 	},
 
 	about: {
-		title: "My name is Mahdi, A student at the Higher School Of Communications Of Tunis.",
+		title: "My name is Mahdi, A student at the Higher School Of Communication Of Tunis.",
 		description:
 			"I've worked on a variety of projects and I'm proud of the progress I've made. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
